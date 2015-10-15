179b63e51531a695ca3e1200e564c422bf0d57a5
Stiliyan Atanasov <stiliyan.atanasov@scalefocus.com>
2015-10-15 14:46:29 +0300

    Added mock service for clients. Fixed indentation issues.

--- app/controllers/clients/clientsCtrl.js
+++ app/controllers/clients/clientsCtrl.js
@@ -1,28 +1,34 @@
-(function () {
-		'use strict';
+(function() {
+  'use strict';
 
-		angular
-			.module('Agrion')
-			.controller('ClientsCtrl', ClientsCtrl);
+  angular
+    .module('Agrion')
+    .controller('ClientsCtrl', ClientsCtrl);
 
-		function ClientsCtrl() {
-			var vm = this;
-      //
-			// // TODO:
-			// vm.sortType = 'name'; // set the default sort type
-			// vm.sortReverse = false; // set the default sort order
-			// vm.searchClient = ''; // set the default search/filter term
-      //
-			// vm.getClients = function () {
-			// 	return db.clients;
-			// };
-      //
-			// vm.edit = function (clientId) {
-			// 	alert(clientId);
-			// };
-      //
-			// vm.delete = function (clientId) {};
+  function ClientsCtrl($http) {
+    var vm = this;
 
-		}
-    
+    // make a get request to obtain all clients
+    // the request url is not valid and currently captured by the mock service
+    $http.get('/api/clients').success(function(data, status, headers) {
+      vm.clients = data;
+    });
+
+    // // TODO:
+    // vm.sortType = 'name'; // set the default sort type
+    // vm.sortReverse = false; // set the default sort order
+    // vm.searchClient = ''; // set the default search/filter term
+    //
+    vm.getClients = function() {
+      return vm.clients;
+    };
+    //
+    // vm.edit = function (clientId) {
+    // 	alert(clientId);
+    // };
+    //
+    // vm.delete = function (clientId) {};
+
+  }
+
 }());
